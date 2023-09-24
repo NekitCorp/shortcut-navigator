14a24478eb401f87b2a389f0a6fc215d7c86e8dc
Nikita Madeev <warhammer-ss@yandex.ru>
2023-09-24 08:28:15 +0300

    feat: :sparkles: Added domain accounting.

--- src/modules/database/index.ts
+++ src/modules/database/index.ts
@@ -1,9 +1,14 @@
 import { UrlParseType } from '../url-manager/types';
+import { withAllDomains } from './domains';
 
 export const DATABASE: Record<Hostname, HostnameOptions> = {
-    'amazon.com': { firstPageNumber: 1, pageDelta: 1, type: UrlParseType.QueryPage },
-    'google.com': { firstPageNumber: 0, pageDelta: 10, type: UrlParseType.QueryStart },
+    ...withAllDomains('amazon', { firstPageNumber: 1, pageDelta: 1, type: UrlParseType.QueryPage }),
+    ...withAllDomains('google', {
+        firstPageNumber: 0,
+        pageDelta: 10,
+        type: UrlParseType.QueryStart,
+    }),
+    ...withAllDomains('ya', { firstPageNumber: 0, pageDelta: 1, type: UrlParseType.QueryP }),
+    ...withAllDomains('yandex', { firstPageNumber: 0, pageDelta: 1, type: UrlParseType.QueryP }),
     'ozon.ru': { firstPageNumber: 1, pageDelta: 1, type: UrlParseType.QueryPage },
-    'ya.ru': { firstPageNumber: 0, pageDelta: 1, type: UrlParseType.QueryP },
-    'yandex.ru': { firstPageNumber: 0, pageDelta: 1, type: UrlParseType.QueryP },
 };
